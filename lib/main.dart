8d2543f2108d4b0cacd0001f5dcddd682bb9ae88
suketupiprotartif <suketu.piprotar@theinternetfolks.com>
2023-08-11 22:55:38 +0530

    server_created

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:amazon_clone/constant/global_variable.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        // primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GLobalVariables.backgroundColor,
+        colorScheme: ColorScheme.light(
+          primary: GLobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: AuthScreen(),
+    );
+  }
+}
